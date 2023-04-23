650a6a9bb8f60174a0fe8131cf1f6048354bb5df
yulia_gorbunova <yuliagorbunova@yahoo.com>
2023-04-23 23:23:07 +0500

    com1

---
+++ sem8.dop/data_create.py
@@ -0,0 +1,15 @@
+def name_data():
+    name = input("Введите имя: ")
+    return name
+
+def lastname_data():
+    lastname = input("Введите фамилию: ")
+    return lastname
+
+def phone_data():
+    phone = input("Введите телефон: ")
+    return phone
+
+def adress_data():
+    adress = input("Введите адрес: ")
+    return adress
